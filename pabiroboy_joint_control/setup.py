fc1798e7f58060d54b8210c805ed8a9e09ffe17a
letrend <simon.trendel@tum.de>
2017-10-19 22:25:20 +0200

    make this install

--- pabiroboy_joint_control/setup.py
+++ pabiroboy_joint_control/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['pabiroboy_joint_control'],
-    package_dir={'': 'src'},
+    package_dir={'': '../'},
     requires=['std_msgs', 'roscpp']
 )
 
--- roboy_motor_command/setup.py
+++ roboy_motor_command/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['roboy_motor_command'],
-    package_dir={'': 'src'},
+    package_dir={'': '../'},
     requires=['std_msgs', 'roscpp']
 )
 
